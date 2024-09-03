859c01ff3c5e55613cd87f8ad29cd1d15a473461
Enmanuel Marval <enmancha@gmail.com>
2024-09-03 19:39:09 -0400

    Agregado value de atributos y clientes. A falta de ventas y reportes.

---
+++ database/migrations/2024_09_03_210459_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string('nacionalidad');
+            $table->string('cedula');
+            $table->string('primer_nombre');
+            $table->string('segundo_nombre');
+            $table->string('primer_apellido');
+            $table->string('segundo_apellido');
+            $table->date('fecha_nacimiento');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
